823bee93d5d7e352fa22f7ebd4f47a18f12a9995
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-04-04 01:02:26 +0600

    52-8 Right Navigation And Info With Icons part-1

--- src/pages/shared/RightSideNav/RightSideNav.jsx
+++ src/pages/shared/RightSideNav/RightSideNav.jsx
@@ -1,7 +1,29 @@
+import { FaFacebookF, FaGithub, FaGoogle } from "react-icons/fa";
 const RightSideNav = () => {
   return (
-    <div>
-      <h2 className="text-2xl">Right Side Nav</h2>
+    <div className="p-4 space-y-4">
+      <div className="space-y-3">
+        <h2 className="text-2xl text-center">Login With</h2>
+        <button className="btn bg-sky-500 w-full text-white">
+          <FaGoogle /> Continue with Google
+        </button>
+        <button className="btn bg-neutral-800 w-full text-white">
+          <FaGithub /> Continue with Google
+        </button>
+      </div>
+      <div className="space-y-3">
+        <h2 className="text-2xl text-center">Find Us On</h2>
+        <div className=" ">
+          <a
+            href="https://facebook.com"
+            target="_blank"
+            className="p-4 flex items-center gap-3"
+          >
+            <FaFacebookF className="text-sky-800 inline" />
+            Facebook
+          </a>
+        </div>
+      </div>
     </div>
   );
 };

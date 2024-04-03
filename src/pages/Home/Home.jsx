c78fe90bd092ff9fa52ea903d2bd0b6e1efbd488
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-04-03 22:38:17 +0600

    52-6 Homepage Layout And Date Time Formatting Using Moment.Js

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,25 @@
 import Header from "../shared/Header/Header";
+import LeftSideNav from "../shared/LeftSideNav/LeftSideNav";
 import Navbar from "../shared/Navbar/Navbar";
+import RightSideNav from "../shared/RightSideNav/RightSideNav";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Navbar />
-      <h2 className="text-3xl font-poppins font-bold ">This is home</h2>
+
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-6 ">
+        <div className=" border-2">
+          <LeftSideNav />
+        </div>
+        <div className="md:col-span-2 border-2">
+          <div className="text-3xl">News Coming Soon</div>
+        </div>
+        <div className=" border-2">
+          <RightSideNav />
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -1,7 +1,11 @@
+import logo from "../../../assets/logo.png";
+import moment from "moment";
 const Header = () => {
   return (
-    <div>
-      <h2 className="text-2xl">this is header</h2>
+    <div className="flex justify-center flex-col items-center gap-3 py-5">
+      <img src={logo} alt="" />
+      <p className="text-xl">Journalism Without Fear or Favour</p>
+      <p className="text-xl">{moment().format("dddd, MMMM D, YYYY ")}</p>
     </div>
   );
 };

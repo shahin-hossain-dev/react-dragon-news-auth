8a68ee4cf852b17158a2decf4977c6ba0fbc7bb7
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-04-05 01:09:21 +0600

    52_5-6 Create Private Route And Do Not Redirect On Reload

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,10 +1,15 @@
+import { useParams } from "react-router-dom";
 import Header from "../shared/Header/Header";
 import RightSideNav from "../shared/RightSideNav/RightSideNav";
+import Navbar from "../shared/Navbar/Navbar";
 
 const News = () => {
+  const { id } = useParams();
+  console.log(id);
   return (
     <div>
       <Header />
+      <Navbar />
       <h2>Dragon News</h2>
       <div className="grid grid-cols-1 lg:grid-cols-4">
         <div className="lg:col-span-3"></div>
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -18,22 +18,27 @@ const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   //   create user
   const createUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
   // sign in user
   const userSignIn = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
 
   //   google login
   const googleLogin = () => {
+    setLoading(true);
     return signInWithPopup(auth, googleProvider);
   };
   // signOut
   const logOut = () => {
+    loading(true);
     return signOut(auth);
   };
   // onAuthStateChange observer
@@ -42,6 +47,7 @@ const AuthProvider = ({ children }) => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       console.log(currentUser);
       setUser(currentUser);
+      setLoading(false);
     });
     return () => {
       unsubscribe();
@@ -50,6 +56,7 @@ const AuthProvider = ({ children }) => {
 
   const authInfo = {
     user,
+    loading,
     createUser,
     userSignIn,
     googleLogin,

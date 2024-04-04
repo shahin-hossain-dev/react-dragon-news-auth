178d202412cf09df90a2370aedce0ce25fcc3c77
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-04-04 20:49:25 +0600

    52_5-1 Clone Existing Projects And Update Remote Origin

--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -12,6 +12,9 @@ const Navbar = () => {
       <li>
         <NavLink to={"/career"}>Career</NavLink>
       </li>
+      <li>
+        <NavLink to={"/login"}>Login</NavLink>
+      </li>
     </div>
   );
   return (

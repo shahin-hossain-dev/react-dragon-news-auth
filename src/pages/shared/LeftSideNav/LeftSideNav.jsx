3eb434706b41a5da986a29197ed96c6b095a9a6a
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-04-04 18:10:11 +0600

    52-9 Left Side Navigation

--- src/pages/shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/shared/LeftSideNav/LeftSideNav.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const LeftSideNav = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("categories.json");
+      const data = await res.json();
+
+      setCategories(data);
+    };
+    fetchData();
+  }, []);
   return (
-    <div>
-      <h2 className="text-2xl">Left Side Nav</h2>
+    <div className="p-4">
+      <h2 className="text-2xl mb-5">All Categories {categories.length}</h2>
+
+      <div className="space-y-4 ">
+        {categories.map((category) => (
+          <Link
+            key={category.id}
+            to={`/category/${category.id}`}
+            className="text-xl block"
+          >
+            {category.name}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
